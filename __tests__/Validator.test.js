abc973eb78daeb14e6c12a3880806d8a777b7907
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-02-13 18:05:12 +0300

    in process

---
+++ __tests__/Validator.test.js
@@ -0,0 +1,37 @@
+import Validator from '../src/js/Validator';
+
+test('name with spaces inside, should pass', () => {
+  const name = 'a b c-';
+  const result = Validator.validateUsername(name);
+  expect(result).toBe(true);
+});
+
+test('name with double spaces inside, should fail', () => {
+  const name = 'a  bc';
+  const result = Validator.validateUsername(name);
+  expect(result).toBe(false);
+});
+
+test('name with space at the beginning, should fail', () => {
+  const name = ' abc';
+  const result = Validator.validateUsername(name);
+  expect(result).toBe(false);
+});
+
+test('name with space at the end, should fail', () => {
+  const name = 'abc ';
+  const result = Validator.validateUsername(name);
+  expect(result).toBe(false);
+});
+
+test('name with non-letter symbols, should fail', () => {
+  const name = 'john5';
+  const result = Validator.validateUsername(name);
+  expect(result).toBe(false);
+});
+
+test('name with two tabs inside, should pass', () => {
+  const name = 'helloworld';
+  const result = Validator.validateUsername(name);
+  expect(result).toBe(true);
+});
