1ab77a4202488dbdb76a3b2d7aebd5ae921f8030
evb0110 <7149553@gmail.com>
2019-02-13 18:05:12 +0300

    in process

--- src/js/Validator.js
+++ src/js/Validator.js
@@ -1,5 +1,12 @@
 class Validator {
-  static validateUsername() {
-    
+  static validateUsername(str) {
+    const regexY = /^\s?[a-zA-ZА-Яа-я -]+\s?$/u;
+    const regexN = /  +|^ | $/;
+
+    return regexY.test(str) && !regexN.test(str);
   }
 }
+
+// export default Validator;
+
+console.log(Validator.validateUsername('hello world'));
